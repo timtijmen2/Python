acb9ee0a0d6498b746675541d1bb1994261c6f79
timtijmen2 <70636605+timtijmen2@users.noreply.github.com>
2020-09-23 14:58:12 +0200

    PlayerStatistics

---
+++ PYTB1L2PlayerStatistics/GameStats.py
@@ -0,0 +1,10 @@
+Nickname = "God"
+SCPNumber = 343
+ObjectClass = 'Safe'
+ability = "Reality bender"
+range = 10.34
+speed = 24
+Geslacht = 'J'
+Inteligentie = 56
+Containt = True
+ContaintCellSize = [6.1, 6.1]
